ecc56ee3e7009d47dc624960cfa67184e7d78e07
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2017-12-23 20:14:06 -0500

    Everything is done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -23,9 +23,9 @@ def max_2_sum(arr)
   end
   for i in 0..length-2
     j = i+1
-    for j in 0..length
+    for j in 0..length-1
       if arr[j] + arr[i] > largest
-        largest = arr[j]+arr[i]
+        largest = arr[j] + arr[i]
       end
     end
   end
@@ -36,41 +36,61 @@ def sum_to_n?(arr, n)
   largest = 0
   length = arr.length
 
-  for i in 0..length
-    if i == n
-      return false
-    end
-  end
-
   for i in 0..length-2
     j = i+1
-    for j in 0..length
+    for j in 0..length-1
       if arr[j] + arr[i] == n
         return true
       end
     end
   end
   return false
-
 end
+
 
 # Part 2
 
 def hello(name)
-  return "Hello, " + name
+  "Hello, " + name
 end
 
 def starts_with_consonant?(s)
   vowels = "AEIOUaeiou"
+  return vowels.include? s[0]
 end
-=begin
+
+
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  unless s == "0"
+      if s=~ /^[0-1]+$/
+        return s.to_i(2) % 4 == 0
+      end
+  end
+  return false
 end
+
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  def isbn
+    @isbn
+  end
+  def price=(price)
+    @price = price
+  end
+  def price
+    @price
+  end
+  def price_as_string
+    format("$%.2f",@price)
+  end
 end
-=end
